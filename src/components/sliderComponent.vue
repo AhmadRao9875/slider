a60bc851bafcee207ea9a26b73a287fdb17f31e4
JsCodexx <muhammadnomi425@gmail.cm>
2023-03-30 22:32:10 +0500

    bootstrap slider

--- src/components/sliderComponent.vue
+++ src/components/sliderComponent.vue
@@ -1,34 +1,24 @@
 <template>
-  <div class="heading"><h1>This is Image slider</h1></div>
   <div class="carousel">
-    <div class="carousel-inner">
-      <transition-group name="slide-in">
-        <div
-          class="carousel-item"
-          :class="{ active: item === currentItem }"
-          v-for="item in items"
-          :key="item.id"
-        >
-          <img :src="item.image" alt="Carousel Item" />
-
-          <div class="carousel-caption">
-            <h3>{{ item.title }}</h3>
-            <p>{{ item.description }}</p>
-          </div>
+    <div
+      class="carousel-inner"
+      :style="{ transform: 'translateX(' + -currentIndex * 100 + '%)' }"
+    >
+      <div
+        v-for="(slide, index) in slides"
+        :key="index"
+        class="carousel-item"
+        :class="{ active: index === currentIndex }"
+      >
+        <img :src="slide.imageSrc" :alt="slide.imageAlt" />
+        <div class="carousel-caption">
+          <h5>{{ slide.title }}</h5>
+          <p>{{ slide.text }}</p>
         </div>
-      </transition-group>
-    </div>
-    <div class="carousel-dots">
-      <span
-        class="dot"
-        :class="{ active: item === currentItem }"
-        v-for="item in items"
-        :key="item.id"
-        @click="setCurrentItem(item)"
-      ></span>
+      </div>
     </div>
     <div class="icon-container">
-      <div class="icon-centering-container" @click="prevItem()">
+      <div class="icon-centering-container" @click="prevSlide()">
         <svg
           fill="none"
           viewBox="0 0 24 24"
@@ -43,7 +33,7 @@
           />
         </svg>
       </div>
-      <div class="icon-centering-container" @click="nextItem()">
+      <div class="icon-centering-container" @click="nextSlide()">
         <svg
           fill="none"
           viewBox="0 0 24 24"
@@ -59,6 +49,15 @@
         </svg>
       </div>
     </div>
+    <div class="carousel-dots">
+      <span
+        v-for="(slide, index) in slides"
+        :key="index"
+        class="carousel-dot"
+        :class="{ active: index === currentIndex }"
+        @click="goToSlide(index)"
+      ></span>
+    </div>
   </div>
 </template>
 
@@ -66,167 +65,97 @@
 export default {
   data() {
     return {
-      items: [
+      slides: [
         {
-          id: 1,
-          title: "First Item",
-          description: "This is the first carousel item",
-          image: "https://picsum.photos/id/1018/1200/800",
+          imageSrc: "https://picsum.photos/id/1015/1200/800",
+          imageAlt: "Image 1",
+          title: "Slide 1",
+          text: "This is the text for slide 1",
         },
         {
-          id: 2,
-          title: "Second Item",
-          description: "This is the second carousel item",
-          image: "https://picsum.photos/id/1015/1200/800",
+          imageSrc: "https://picsum.photos/id/1018/1200/800",
+          imageAlt: "Image 2",
+          title: "Slide 2",
+          text: "This is the text for slide 2",
         },
         {
-          id: 3,
-          title: "Third Item",
-          description: "This is the third carousel item",
-          image: "https://picsum.photos/id/1019/1200/800",
+          imageSrc: "https://picsum.photos/id/1019/1200/800",
+          imageAlt: "Image 3",
+          title: "Slide 3",
+          text: "This is the text for slide 3",
         },
       ],
-      currentItem: null,
+      currentIndex: 0,
     };
   },
-  mounted() {
-    // displays the item 0 on the start,
-    this.currentItem = this.items[0];
-  },
   methods: {
-    nextItem() {
-      const currentIndex = this.items.indexOf(this.currentItem);
-      console.log(currentIndex, "this is currentIndex");
-      this.currentItem =
-        // if the current item is the last item of the array ,it will set the currentItem to 0 else to the next
-        currentIndex === this.items.length - 1
-          ? this.items[0]
-          : this.items[currentIndex + 1];
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.slides.length;
     },
-    prevItem() {
-      const currentIndex = this.items.indexOf(this.currentItem);
-      this.currentItem =
-        currentIndex === 0
-          ? this.items[this.items.length - 1]
-          : this.items[currentIndex - 1];
+    prevSlide() {
+      this.currentIndex =
+        (this.currentIndex - 1 + this.slides.length) % this.slides.length;
     },
-    setCurrentItem(item) {
-      // this will display the the item wchic is clicked
-      this.currentItem = item;
+    goToSlide(index) {
+      this.currentIndex = index;
     },
   },
 };
 </script>
 
-<style>
-.heading {
-  display: flex;
-  justify-content: center;
-  background-color: antiquewhite;
-}
+<style scoped>
 .carousel {
-  background-color: rgb(233, 231, 231);
   position: relative;
-  margin: 0 auto;
   width: 100%;
-  max-width: 75rem;
-  height: 100%;
+  overflow: hidden;
 }
 
 .carousel-inner {
-  position: relative;
-  overflow: hidden;
   width: 100%;
   height: 100%;
-  padding-bottom: 60%;
+  display: flex;
+  transition: transform 0.5s ease-in-out;
 }
 
 .carousel-item {
-  /* position: absolute;
-  top: 0;
-  left: 0;
+  flex-shrink: 0;
+  position: relative;
   width: 100%;
-  height: 100%;
-  opacity: 0;
-  transition: transform 0.5s ease-in-out; */
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  right: -100%;
-  opacity: 1;
-  transition: all 1.5s;
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  align-items: center;
-  transform: translateX(0);
 }
+
 .carousel-item img {
   width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-
-/* .carousel-item.active {
-  right: 0;
-  opacity: 1;
-  transition: 1s; 
-  transition: opacity 1s ease-in-out;
-} */
-.active {
-  /* transform: translateX(-100%); */
-  right: 0;
-  opacity: 1;
-  transition: opacity 1.5s ease-in-out;
-}
-.carousel-item.next {
-  transform: translateX(100%);
-}
-
-.carousel-item.prev {
-  transform: translateX(-100%);
+  max-height: 45rem;
 }
 
 .carousel-caption {
   position: absolute;
-  bottom: 20%;
+  top: 50%;
   left: 50%;
-  transform: translateX(-50%);
-  text-align: center;
+  padding: 15px;
   color: #fff;
-}
-
-.carousel-caption h3 {
-  font-size: 2rem;
-  margin-bottom: 1.8rem;
-}
-
-.carousel-caption p {
-  font-size: 1.8rem;
-  line-height: 1.5;
 }
 
 .carousel-dots {
   position: absolute;
   display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 90%;
+  top: 95%;
   left: 50%;
+  justify-content: center;
+  /* margin-top: 10px; */
 }
 
-.carousel-dots .dot {
-  display: inline-block;
+.carousel-dot {
   width: 15px;
   height: 15px;
-  margin: 0 5px;
   border-radius: 50%;
   background-color: #bbb;
+  margin: 0 5px;
   cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
 }
 
-.carousel-dots .dot.active {
+.carousel-dot.active {
   background-color: #fff;
   width: 10px;
   height: 10px;
@@ -262,15 +191,5 @@ export default {
 .icon--left {
   left: 0;
   transform: translate(-50%, -50%);
-}
-.slide-in-enter-active,
-.slide-in-leave-active {
-  transition: all 1s ease-in-out;
-}
-.slide-in-enter-from {
-  transform: translateX(-100%);
-}
-.slide-in-leave-to {
-  transform: translateX(100%);
 }
 </style>
